5d6613ac68f34a559438546ea0423c328a85579a
David-OConnor <Lowacid2@>
2015-07-30 19:48:27 +0000

    Added more links, cleaned up others, modified map entries.

--- app/views.py
+++ app/views.py
@@ -7,26 +7,30 @@ from app import app
 Link = namedtuple('Link', ['title', 'url'])
 Label = namedtuple('Label', ['text', 'lat', 'lon'])
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     # todo consider making this a dict, to prevent repeating code
     # todo in the template. Only if the format stays the same for all/most cats.
     ops_resources = [
-        Link("OGV home page (Gov computer only)",
+        Link("OGV home page*",
              'https://ice.usafe.af.mil/sites/48FW/48thOperationsGroup/48OGStan'
              'Eval/SitePages/Home.aspx'),
-        Link("OGV pubs library (Gov computer only)", 
+        Link("OGV pubs library*",
              'https://ice.usafe.af.mil/sites/48FW/48thOperationsGroup/48OGStan'
              'Eval/FCIF%20(Pubs)%20Library/SitePages/Home.aspx'),
+        Link("OGV Master Question  Files*",
+             'https://ice.usafe.af.mil/sites/48FW/48thOperationsGroup/'
+             '48OGStanEval/MQF/F15E/Forms/AllItems.aspx'),
         # Sub links on the main page to pubs checkers, ifg etc?
         Link("Lowfly Notams and Target imagery", 'http://flight-plan.herokuapp.com/'),
-        Link("Ops useful contacts", '/contacts'),
+        # Link("Ops useful contacts", '/contacts'),
     ]
 
     personal = [
-        Link("Pass policy", '/pass'),
-        Link("Form 6s (Foreign travel)", ''),
+        #Link("Pass policy", '/pass'),
+        Link("Form 6s (Foreign travel)*", ''),
         Link("Leave", 'https://www.my.af.mil/leavewebprod/profile'),
         Link("Pay", 'https://mypay.dfas.mil/mypay.aspx')
     ]
@@ -35,6 +39,7 @@ def index():
         Link("Base map", '/map'),
         Link("Official useful contacts",
              'http://www.lakenheath.af.mil/news/usefulcontacts.asp'),
+        Link("Base dining menus and hours", 'http://www.lakenheathfss.com/dining/')
     ]
 
     queep = [
@@ -77,20 +82,24 @@ def base_map():
 
     labels = [
         Label("SEC", 52.399423, 0.561185),
-        Label("Hangar 6 (events)", 52.402678, 0.560025),
+        Label("Sims", 52.398394, 0.563266),
+        Label("Hangar 6", 52.402678, 0.560025),
         Label("MOPP training", 52.399567, 0.565717),
-        #Label("SERE", 52.400139, 0.567896),
-        #Label("Gold", ),
+        # Label("SERE", 52.400139, 0.567896),
+        Label("Gold", 52.410059, 0.578290),
         Label("Post office", 52.397489, 0.541705),
-        Label("Golf course/Food", 52.398476, 0.569096),
+        Label("Golf course, Breckland Pines", 52.398476, 0.569096),
         Label("Gym", 52.392908, 0.540615),
         Label("MPF", 52.395588, 0.547411),
+        Label("Comm", 52.399222, 0.548833),
         Label("Flight medicine", 52.394161, 0.544522),
         Label("Lodging", 52.393930, 0.548817),
         Label("BX", 52.392778, 0.547357),
+        Label("Uniform store", 52.391725, 0.544996),
+        Label("Commissary", 52.396789, 0.550654),
         Label("Auto hobby", 52.398453, 0.544610),
         Label("Auto mechanic", 52.396730, 0.542175),
-        Label("Liquor store, subway", 52.390869, 0.544623),
+        Label("Liquor store, Subway", 52.390869, 0.544623),
         Label("Rugby's, Pinkerton's", 52.390250, 0.544795),
         Label("Pizza", 52.390568, 0.545648),
         Label("Fast food", 52.396392, 0.545290),
