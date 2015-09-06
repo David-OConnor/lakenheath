5f66e8988098c9f67ee89e37533373e2472aeca8
David-OConnor <Lowacid2@>
2015-09-06 00:15:33 +0000

    added logging; troubleshootingi heroku push

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -10,11 +13,12 @@ app.config.from_object('app.settings')
 db = SQLAlchemy(app)
 admin = Admin(app, name='Lakenlink', template_mode='bootstrap3')
 
-# import the models here to prevent chicken-egg import problems.
-from app.models import User, Panther
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Panther, db.session))
-
 heroku = Heroku(app)
 
 from app import views, models
+
+admin.add_view(ModelView(models.User, db.session))
+admin.add_view(ModelView(models.Panther, db.session))
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
