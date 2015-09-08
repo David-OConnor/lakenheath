f46450fb9bdef47bb75a6198c6483a00b2a6d881
David-OConnor <Lowacid2@>
2015-09-08 15:38:58 +0000

    added script to make first admin

---
+++ create_admin.py
@@ -0,0 +1,23 @@
+from flask_security.utils import encrypt_password
+from flask_security import SQLAlchemyUserDatastore
+
+from app import app
+from app import db
+
+from app.models import Role, User
+
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+
+with app.app_context():
+    super_user_role = Role(name='superuser')
+    user_role = Role(name='user')
+
+    first_admin = user_datastore.create_user(
+        email='admin2',
+        password=encrypt_password('admin'),
+        roles=[user_role, super_user_role]
+    )
+    
+db.session.add(first_admin)
+db.session.commit()
