07619465953db5bfcdccccc428d713f2521927ee
David-OConnor <Lowacid2@>
2015-09-09 22:35:18 +0000

    Troubleshooting an SSl workaround pt6.9

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ app.config.from_object('app.config')
 # lakenheath.herokuapp website, to avoid paying for an SSL cert etc.
 # sslify = SSLify(app, skips=['/', '/index', '/map', '/admin', '/login', '/register',
 #                             '/logout'])
-sslify = SSLify(app, skips=[''])
+sslify = SSLify(app, skips=['index'])
 
 db = SQLAlchemy(app)
 admin = Admin(app, name='Lakenlink', base_template='my_master.html',
